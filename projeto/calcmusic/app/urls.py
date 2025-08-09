b2b05b7ea75e59161fa31369a9b7440daa8199dc
Sandro <sandrocoelho2023a@gmail.com>
2025-08-09 14:40:32 -0300

    autenticação de usuários

--- projeto/calcmusic/app/urls.py
+++ projeto/calcmusic/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.index, name="index"),
+    path("login/", views.login_v, name="login"),
+    path("logout/", views.logout_v, name="logout"),
+
 ]
--- projeto/calcmusic/app/views.py
+++ projeto/calcmusic/app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# códigos inciais de autenticação
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request, "app/index.html")
+
+
+def login_v(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        return render(request, "app/login.html", {"erro": "Usuário ou senha invalidos"})
+    return render(request, "app/login.html")
+
+
+def logout_v(request):
+    logout(request)
+    return render(request, "app/login.html", {"erro": "Usuário não logado"})

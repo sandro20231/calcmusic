2649d28cad146566a4788aa85e9253a54fcdc8bc
Sandro <sandrocoelho2023a@gmail.com>
2025-08-11 20:19:53 -0300

    adicionando testes

--- projeto/calcmusic/app/tests.py
+++ projeto/calcmusic/app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Comentarios
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class TestesMusicais(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_index(self):
+        c= Client()
+        resposta = c.get("/calculadora/")
+        self.assertEqual(resposta.status_code, 200)
